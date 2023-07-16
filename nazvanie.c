7dc1951358eb2b6cc3c673c61d7cae491321731a
UTAfairy <test.kt.6606@gmail.com>
2023-07-16 03:49:17 +0300

    да блин отправляйся

---
+++ nazvanie.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <locale.h>
+int main(void)
+{
+      int number1 = 1, number2 = 666, number3 = 3;
+    printf("number1 = %d \n", number1);
+    printf("number2 = %d \n", number2);
+    printf("number3 = %d \n", number3);
+    return 0;
+}
